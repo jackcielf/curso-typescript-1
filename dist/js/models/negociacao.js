961e5b09e70e5883f94922c131b93a291ca4cc0f
JakkiFx <jackcielfelix555@gmail.com>
2023-10-06 10:01:18 -0300

    aula 2

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -12,12 +12,16 @@ export class Negociacao {
     get data() {
         return this.#data;
     }
-    
+
     get quantidade() {
         return this.#quantidade;
     }
 
     get valor() {
         return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
     }
 }
